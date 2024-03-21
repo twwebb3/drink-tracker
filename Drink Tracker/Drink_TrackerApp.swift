9321903524633d3fcaa3bed1257803f727a22f9b
TW Webb <twwebb1@me.com>
2024-03-21 16:16:04 -0400

    Initial Commit

---
+++ Drink Tracker/Drink_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drink_TrackerApp.swift
+//  Drink Tracker
+//
+//  Created by Theodore Webb on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Drink_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Drink TrackerUITests/Drink_TrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Drink_TrackerUITestsLaunchTests.swift
+//  Drink TrackerUITests
+//
+//  Created by Theodore Webb on 3/21/24.
+//
+
+import XCTest
+
+final class Drink_TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
